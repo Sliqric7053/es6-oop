181638dac9c0095ef5baa874ff58133b55fbf68c
Richard Sithole <richardsit@gmail.com>
2019-06-30 09:33:31 +0200

    Clean up

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import { Image } from './ui/image';
 import { TitleBar } from './ui/title-bar';
 import { FleetDataService } from './services/fleet-data-service';
 import { DataTable } from './ui/data-table';
+import { fleet } from './fleet-data';
 
 let body = document.querySelector('body');
 
-// let btn = new Button('Click Me!');
-// let image = new Image(drone1);
 let title = new TitleBar('AI Vehicles');
-
-// btn.appendToElement(body);
-// image.appendToElement(body);
 
 title.addLink('Home', '');
 title.addLink('Cars', '');
@@ -24,8 +20,6 @@ title.appendToElement(body);
 let headers = 'License Make Model Miles'.split(' ');
 let dataService = new FleetDataService();
 console.log('TCL: dataService', dataService);
-dataService.loadData('car');
+dataService.loadData(fleet);
 let dt = new DataTable(headers, dataService.cars);
-// console.log('TCL: dataService.cadrors', dataService.drones);
-// console.log('TCL: dt', dt);
 dt.appendToElement(body);
